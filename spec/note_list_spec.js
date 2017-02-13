38dc4f104d618578b7a4ecf0ecba548327b792ca
olwend <olwend@gmail.com>
2017-02-13 20:45:28 +0000

    returns array[0] for test

--- spec/note_list_spec.js
+++ spec/note_list_spec.js
@@ -4,8 +4,11 @@ function testNewNoteList(){
   addNoteList(first);
   addNoteList(second);
   if (checkArray() != 2) {
-    throw new Error("notelist does not have the expected notes");
-  }
+    throw new Error("notelist is not expected length");
+    }
+ if (returnArray() != "cat") {
+    throw new Error("notelist content is incorrect");
+    }
 };
 
 
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -4,10 +4,15 @@
     return notelist.push(model);
   }
   function checkArray(){
-    console.log(notelist)
     console.log (notelist.length)
     return notelist.length;
   }
+  function returnArray(){
+    console.log(notelist)
+    console.log(notelist[0])
+    return notelist[0];
+  }
   exports.addNoteList = addNoteList;
   exports.checkArray = checkArray;
+  exports.returnArray = returnArray;
 })(this);
